9148d19530f782716bb1d60b0d4f0fa15fa6923e
Junaya <ayac3j@gmail.com>
2022-03-16 23:44:24 +0800

    feat: button props

--- src/components/button.rs
+++ src/components/button.rs
@@ -7,10 +7,11 @@ pub struct ButtonProps {
 }
 
 #[warn(non_snake_case)]
+#[allow(dead_code)]
 pub fn Button(cx: Scope<ButtonProps>) -> Element {
     cx.render(rsx!(
         button {
-            "cx.props.text"
+            "{cx.props.text}"
         }
     ))
 }
