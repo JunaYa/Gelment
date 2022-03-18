1f22f8ed01f7c9a9f4284b76cfcb3f5f18839d04
YajunWang <wangyajun@hetao101.com>
2022-03-18 16:00:09 +0800

    feat: update checkbox check

--- src/components/checkbox.rs
+++ src/components/checkbox.rs
@@ -26,11 +26,36 @@ pub struct CheckboxProps<'a> {
 
 pub fn Checkbox<'a>(cx: Scope<'a, CheckboxProps<'a>>) -> Element{
 	let color = cx.props.color.text_color();
+	let cursor = if cx.props.disabled == true { "not-allowed" } else { "pointer" };
+	let mut checked = cx.use_hook(|_| false);
+
 	cx.render(rsx!{
 		div {
 			display: "inline-flex",
+			justify_content: "center",
+			align_items: "center",
+			cursor: "{cursor}",
+			onclick: move |evt| {
+				println!("before {}", checked);
+				cx.props.onclick.call(evt);
+				*checked = match checked {
+					true => false,
+					false => true,
+				};
+				println!("after {}", checked);
+			},
+			div {
+				width: ".8rem",
+				height: ".8rem",
+				border: ".15rem solid {color}",
+				border_radius: ".3rem",
+				background_color: "#fff",
+				margin_right: ".4rem",
+				box_shadow: "0.15rem 0.15rem 0.3rem #c8d0e7, -0.1rem -0.1rem 0.25rem #FFFFFF",
+			}
 			input {
 				display: "none",
+				r#type: "checkbox",
 				checked: "{cx.props.checked}",
 			}
 			label {
