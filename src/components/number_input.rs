50282cc1ba05a88b9d930d10aae0aefd8b6b6351
JunaYa <ayac3j@gmail.com>
2022-04-05 01:31:30 -0600

    feat: update number input style

--- src/components/number_input.rs
+++ src/components/number_input.rs
@@ -18,31 +18,28 @@ pub fn NumberInput<'a>(cx: Scope<'a, NumberInputProps<'a>>) -> Element {
             display: "inline-flex",
             flex_direction: "row",
             align_items: "center",
-            justify_content: "right",
+            justify_content: "center",
             padding: "0.2rem",
             background_color: "white",
-            width: "100%",
             height: "2rem",
             margin: "0.5rem",
             border_radius: "0.5rem",
             cursor: "pointer",
             div {
                 align_items: "flex-start",
-                width: "100%",
-                height: "100%",
                 border_radius: "0.5rem",
                 background_color: "white",
+                margin_right: "0.5rem",
                 "{label}"
             }
             input {
-                width: "100%",
-                height: "100%",
                 border: "none",
                 text_align: "center",
                 font_size: "1.5rem",
                 font_weight: "bold",
                 color: "black",
-                background_color: "transparent",
+                background_color: "red",
+                text_align: "center",
                 oninput: move |evt| {
                     println!("{:?}", evt);
                 },
