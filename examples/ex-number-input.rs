5feb1c71a6731dad3eb1b5e64f0ac1d5cfedcc95
JunaYa <ayac3j@gmail.com>
2022-04-08 10:22:26 -0600

    feat: ex-number-input colors

--- examples/ex-number-input.rs
+++ examples/ex-number-input.rs
@@ -1,39 +1,12 @@
 use dioxus::prelude::*;
-use gelement::{prelude::*, size::Size, color::Color};
+use gelement::{prelude::*, size::Size, color::Color, constants::getColors};
 
 fn main() {
     dioxus::desktop::launch(app);
 }
 
 fn app(cxt: Scope) -> Element {
-    let v_color: Vec<Color> = vec![
-        Color::Transparent,
-        Color::Black,
-        Color::White,
-        Color::Slate,
-        Color::Gray,
-        Color::Zinc,
-        Color::Neutral,
-        Color::Stone,
-        Color::Red,
-        Color::Orange,
-        Color::Amber,
-        Color::Yellow,
-        Color::Lime,
-        Color::Green,
-        Color::Emerald,
-        Color::Teal,
-        Color::Cyan,
-        Color::Sky,
-        Color::Blue,
-        Color::Indigo,
-        Color::Violet,
-        Color::Purple,
-        Color::Fuchsia,
-        Color::Pink,
-        Color::Rose,
-        Color::Default,
-    ];
+    let v_color: Vec<Color> = getColors();
     cxt.render(rsx! (
         v_color.iter().map(|color| {
             rsx!(
