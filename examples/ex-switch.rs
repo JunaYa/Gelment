386fa10251a1d4301e21fce3d99a3d3747cb303d
JunaYa <ayac3j@gmail.com>
2022-04-08 10:24:10 -0600

    feat: ex-switch colors

--- examples/ex-switch.rs
+++ examples/ex-switch.rs
@@ -3,6 +3,7 @@ use gelement::{
     prelude::*,
     size::Size,
     color::Color,
+    constants::getColors,
 };
 
 fn main() {
@@ -11,34 +12,7 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let v_color = vec![
-        Color::Transparent,
-        Color::Black,
-        Color::White,
-        Color::Slate,
-        Color::Gray,
-        Color::Zinc,
-        Color::Neutral,
-        Color::Stone,
-        Color::Red,
-        Color::Orange,
-        Color::Amber,
-        Color::Yellow,
-        Color::Lime,
-        Color::Green,
-        Color::Emerald,
-        Color::Teal,
-        Color::Cyan,
-        Color::Sky,
-        Color::Blue,
-        Color::Indigo,
-        Color::Violet,
-        Color::Purple,
-        Color::Fuchsia,
-        Color::Pink,
-        Color::Rose,
-        Color::Default,
-    ];
+    let v_color = getColors();
     cx.render(rsx!(
         v_color.iter().map(move |color| {
             rsx!(
