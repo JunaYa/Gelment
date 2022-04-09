965905c36a8e1dd92129823f6556a2730a2033a1
JunaYa <ayac3j@gmail.com>
2022-04-09 09:06:50 -0600

    feat: add text input

--- src/components/text_input.rs
+++ src/components/text_input.rs
@@ -1,10 +1,31 @@
 use dioxus::prelude::*;
 
 #[derive(Props, PartialEq)]
-struct TextInputProps {
+pub struct TextInputProps {
     #[props(default)]
     color: Color,
 
     #[props(default)]
     size: Size,
+
+    #[props(default)]
+    value: &str,
+}
+
+pub fn TextInput(cx: Scope<TextInputProps>) -> Element {
+    cx.render(rsx!(
+        input {
+            "type": "text",
+            value: "{search_input}",
+            placeholder: "placeholder",
+            oninput: move |evt| {
+                println!("{:?}", evt);
+            },
+            onkeydown: move |evt| {
+                println!("{:?}", evt);
+                if evt.key == "Enter" {
+                }
+            }
+        }
+    ))
 }
