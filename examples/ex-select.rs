484a0116ee4ace6c69f6a10d7e611039c7828395
JunaYa <ayac3j@gmail.com>
2022-04-11 22:33:13 +0800

    feat: add ex-select

---
+++ examples/ex-select.rs
@@ -0,0 +1,69 @@
+use dioxus::prelude::*;
+use gelement::{prelude::*, size::Size, color::Color, constants::getColors};
+
+
+fn main() {
+    // Add this line:
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let v_color: Vec<Color> = getColors();
+    cx.render(rsx!(
+        v_color.iter().map(|color| {
+            rsx!(
+                div {
+                    Select{
+                        checked: false,
+                        color: *color,
+                        size: Size::TINY,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                    Select{
+                        checked: true,
+                        color: *color,
+                        size: Size::SMALL,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                    Select{
+                        checked: true,
+                        color: *color,
+                        size: Size::MEDIUM,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                    Select{
+                        checked: true,
+                        color: *color,
+                        size: Size::LARGE,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                    Select{
+                        checked: true,
+                        color: *color,
+                        size: Size::XLARGE,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                    Select{
+                        disabled: true,
+                        checked: false,
+                        color: *color,
+                        size: Size::XXLARGE,
+                        onclick: move |_| {
+                            println!("radio click")
+                        }
+                    }
+                }
+            )
+        })
+    ))
+}
