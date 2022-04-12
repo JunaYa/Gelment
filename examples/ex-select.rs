8ae10a9f305d87169e16643752ff163a96b32c30
JunaYa <ayac3j@gmail.com>
2022-04-12 22:44:58 +0800

    feat: update ex-select

--- examples/ex-select.rs
+++ examples/ex-select.rs
@@ -9,6 +9,7 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
     let v_color: Vec<Color> = getColors();
+    // let options = vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")];
     cx.render(rsx!(
         v_color.iter().map(|color| {
             rsx!(
@@ -17,31 +18,37 @@ fn app(cx: Scope) -> Element {
                         disabled: false,
                         color: *color,
                         size: Size::TINY,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                     Select{
                         disabled: false,
                         color: *color,
                         size: Size::SMALL,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                     Select{
                         disabled: false,
                         color: *color,
                         size: Size::MEDIUM,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                     Select{
                         disabled: false,
                         color: *color,
                         size: Size::LARGE,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                     Select{
                         disabled: false,
                         color: *color,
                         size: Size::XLARGE,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                     Select{
                         disabled: false,
                         color: *color,
                         size: Size::XXLARGE,
+                        options: vec![("1", "Medium"), ("2", "Small"), ("3", "Large"), ("4", "XLarge"), ("5", "XXLarge")],
                     }
                 }
             )
